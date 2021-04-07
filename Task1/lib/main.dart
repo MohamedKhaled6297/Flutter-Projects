c7aee3a50d7129eaf526b2d6644e1a49c24285b8
MohamedKhaled2268 <75572519+MohamedKhaled2268@users.noreply.github.com>
2021-04-07 12:50:13 +0200

    Update main.dart

--- Task1/lib/main.dart
+++ Task1/lib/main.dart
@@ -1,58 +1,18 @@
 void main(List<String> args) {
-  //int age = 24;
-  // print(age+5);
-  // String s = 'Ahmed';
-  // print(s);
-  // bool b = true;
-  // print(b == false);
-  // int a = 3;
-  // int b = 0;
-  // b = a++;
-  // print(a);
-  // print(b);
-  // int a2 = 3;
-  // int b2 = 0;
-  // b2 = ++a2;
-  // print(a2);
-  // print(b2);
-  // List arr = [1, 2, 3];
-  // for(int i =0; i< arr.length; i++){
-  //   print(arr[i]);
-  // }
-  // print(arr[1]);
-  // arr.add(4);
-  // print(arr);
-  // arr.remove(2);
-  // print(arr);
-  // arr.remove(5);
-  // print(arr);
-  // Map map = {'a' : [1,2,3], 'b': 2};
-  // print(map['a'][0]);
-
-  int degree = 49;
-  if(degree >= 50){
-    print("suceess");
-  }
-  else{
-    print("Failed");
-  }
-  // print(getName());
-  // User user = new User();
-  // print(user.name);
-  // print(user.getName());
-
-  // User1 user1 = User1();
-  // print(user1.getName());
+  int age = 24;
+  print(age+5);
+  String s = 'Ahmed';
+  print(s);
+  bool b = true;
+  print(b == false);
+  int a = 3;
+  int b = 0;
+  b = a++;
+  print(a);
+  print(b);
+  int a2 = 3;
+  int b2 = 0;
+  b2 = ++a2;
+  print(a2);
+  print(b2);
 }
-
-String getName() => "Dalia";
-
-class User{
-  String name = "MD";
-  String getName() => "Da";
-}
-class User1 extends User{
-  User1() : super();
-  // String getName() => "UUU";
-}
-// list + for loop + method
