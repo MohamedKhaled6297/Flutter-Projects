582efdfb5f7051f07dd05849ea6b792cfd705835
MohamedKhaled2268 <75572519+MohamedKhaled2268@users.noreply.github.com>
2021-04-08 10:56:46 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,44 +15,7 @@ class MyFirstApplication extends StatelessWidget {
         title: Text('My First Application'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          // crossAxisAlignment: ,
-          // mainAxisAlignment: ,
-          children: <Widget>[
-            Text(
-              'Hello Dalia',
-              style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.purple
-              ),
-            ),
-            // SizedBox(width: 10.0),
-            RaisedButton(
-              onPressed: (){
-                print('You Cliced Me');
-              },
-              child: Text('click Me'),
-            ),
-            RaisedButton(
-              onPressed: (){
-                print('You Cliced Me');
-              },
-              child: Text('click Me'),
-            ),
-            Text(
-              'Hello Dalia',
-              style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.purple
-              ),
-            ),
-          ],
-        )
-      ),
-
+      body: Text('Hello Dalia'),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
         child: Icon(Icons.add),
@@ -61,12 +24,3 @@ class MyFirstApplication extends StatelessWidget {
     );
   }
 }
-/*
- font family
- image ....
- Row
- Column
- */
-
-// color floatin action button
-// color appbar
