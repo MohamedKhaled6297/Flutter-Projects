82df6afb024f89e5eda7ad1f1b66e24d5e889b05
Mohamed Khaled Ahmed <75572519+MohamedKhaled6297@users.noreply.github.com>
2022-10-25 14:35:45 +0200

    first Lecture

---
+++ main.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+  int age = 24;
+  print(age+5);
+  String s = 'Ahmed';
+  print(s);
+  bool b = true;
+  print(b == false);
+  dynamic a = 22;
+  print(a++);
+  a = 's ';
+  print(a + 'as');
+  
+  int a = 3;
+  int b = 0;
+  b = a++;
+  print(a);
+  print(b);
+  int a2 = 3;
+  int b2 = 0;
+  b2 = ++a2;
+  print(a2);
+  print(b2);
+  
+}
